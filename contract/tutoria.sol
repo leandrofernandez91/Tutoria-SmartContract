c8cfcbc59f904b5f04faa3197425353589b9f4d4
Leandro L. Fernandez Avancini <leandrofernandez91@gmail.com>
2018-10-17 11:54:34 -0300

    Conexion el servidor
    Se logró conectar el remix al servidor local del ganache.
    Se implemento el mapping con hash en tipo bytes32 sin necesidad de conversion a uint256

---
+++ contract/tutoria.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.4.7;
+contract Tutoria {
+
+    struct tutoriaData{
+        string materia;
+        address idProfesor;
+        address alumno;
+        uint confirmado;
+        uint cancelado;
+        uint256 hash;
+
+
+    }
+
+    tutoriaData[] public tutoConf;
+    tutoriaData[] public tutoCanc;
+
+    mapping (uint256 => tutoriaData) tutorias;
+
+
+    function solicitar (string _materia, address _idProfesor) public returns(uint256){
+        require(_idProfesor != msg.sender);
+        uint256 hashAux = uint256(keccak256(_materia, _idProfesor));
+        tutoriaData t = tutorias[hashAux];
+        t.alumno = msg.sender;
+        t.materia = _materia;
+        t.idProfesor = _idProfesor;
+        t.confirmado = 0;
+        t.cancelado = 0;
+        t.hash = hashAux;
+        //tuto.push(t);
+        return t.hash;
+    }
+
+
+    function getMateria(uint256 key) public view returns (string) {
+        tutoriaData t = tutorias[key];
+        return t.materia;
+    }
+
+    function getHash(uint256 key) public view returns (uint256) {
+        tutoriaData t = tutorias[key];
+        return t.hash;
+    }
+
+    function getIdProfesor(uint256 key) public view returns (address) {
+        tutoriaData t = tutorias[key];
+        return t.idProfesor;
+    }
+
+    function getAlumno(uint256 key) public view returns (address) {
+        tutoriaData t = tutorias[key];
+        return t.alumno;
+    }
+
+    function confirmar(uint256 key) public{
+        tutoriaData storage t = tutorias[key];
+        require(msg.sender == t.idProfesor);
+        t.confirmado = 1;
+        tutoConf.push(t);
+    }
+
+    function cancelar(uint256 key) public{
+        tutoriaData t = tutorias[key];
+        require(msg.sender == t.alumno);
+        t.cancelado = 1;
+        tutoCanc.push(t);
+    }
+
+    function estaConfirmado(uint256 key) public view returns (uint) {
+        tutoriaData t = tutorias[key];
+        return t.confirmado;
+    }
+
+    function estaCancelado(uint256 key) public view returns (uint){
+        tutoriaData t = tutorias[key];
+        return t.cancelado;
+    }
+
+}
--- tutoria.sol
+++ tutoria.sol
@@ -7,20 +7,20 @@ contract Tutoria {
         address alumno;
         uint confirmado;
         uint cancelado;
-        uint256 hash;
+        bytes32 hash;
 
 
     }
-
+    tutoriaData[] public tuto;
     tutoriaData[] public tutoConf;
     tutoriaData[] public tutoCanc;
 
-    mapping (uint256 => tutoriaData) tutorias;
+    mapping (bytes32 => tutoriaData) tutorias;
 
 
-    function solicitar (string _materia, address _idProfesor) public returns(uint256){
+    function solicitar (string _materia, address _idProfesor) public returns(bytes32){
         require(_idProfesor != msg.sender);
-        uint256 hashAux = uint256(keccak256(_materia, _idProfesor));
+        bytes32 hashAux = keccak256(_materia, _idProfesor);
         tutoriaData t = tutorias[hashAux];
         t.alumno = msg.sender;
         t.materia = _materia;
@@ -28,37 +28,57 @@ contract Tutoria {
         t.confirmado = 0;
         t.cancelado = 0;
         t.hash = hashAux;
-        //tuto.push(t);
-        return t.hash;
+        tuto.push(t);
+        return hashAux;
     }
 
 
-    function getMateria(uint256 key) public view returns (string) {
+    function getMateria(bytes32 key) public view returns (string) {
         tutoriaData t = tutorias[key];
         return t.materia;
     }
 
-    function getHash(uint256 key) public view returns (uint256) {
+    function getHash(bytes32 key) public view returns (bytes32) {
         tutoriaData t = tutorias[key];
         return t.hash;
     }
 
-    function getIdProfesor(uint256 key) public view returns (address) {
+    function getIdProfesor(bytes32 key) public view returns (address) {
         tutoriaData t = tutorias[key];
         return t.idProfesor;
     }
 
-    function getAlumno(uint256 key) public view returns (address) {
+    function getAlumno(bytes32 key) public view returns (address) {
         tutoriaData t = tutorias[key];
         return t.alumno;
     }
 
-    function confirmar(uint256 key) public{
+    function confirmar(bytes32 key) public{
         tutoriaData storage t = tutorias[key];
         require(msg.sender == t.idProfesor);
         t.confirmado = 1;
         tutoConf.push(t);
     }
+
+    function cancelar(bytes32 key) public{
+        tutoriaData t = tutorias[key];
+        require(msg.sender == t.alumno);
+        t.cancelado = 1;
+        tutoCanc.push(t);
+    }
+
+    function estaConfirmado(bytes32 key) public view returns (uint) {
+        tutoriaData t = tutorias[key];
+        return t.confirmado;
+    }
+
+    function estaCancelado(bytes32 key) public view returns (uint){
+        tutoriaData t = tutorias[key];
+        return t.cancelado;
+    }
+
+}
+
 
     function cancelar(uint256 key) public{
         tutoriaData t = tutorias[key];
